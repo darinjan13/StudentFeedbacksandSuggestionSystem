8f9735f24b8215606ba854830f7ea243dc43abc3
darinjan13 <darinjan13@gmail.com>
2024-05-22 02:10:58 +0800

    C

--- StudentFeedbacksandSuggestionSystem/Properties/Settings.Designer.cs
+++ StudentFeedbacksandSuggestionSystem/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace StudentFeedbacksandSuggestionSystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace StudentFeedbacksandSuggestionSystem.Properties {
         public string StudentFeedbackAndSuggestionsDBConnectionString {
             get {
                 return ((string)(this["StudentFeedbackAndSuggestionsDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\StudentSuggestionsM" +
+            "anagementDB.accdb")]
+        public string StudentSuggestionsManagementDBConnectionString {
+            get {
+                return ((string)(this["StudentSuggestionsManagementDBConnectionString"]));
             }
         }
     }
